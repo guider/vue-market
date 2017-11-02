b3a9a72d1f2cbc6f88ce6dc4355f92442118cb6a
guider <guider@yeah.net>
2017-11-02 19:35:37 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <wheel-view></wheel-view>
+  </div>
+</template>
+
+<script>
+
+  import WheelView from "./wheel/Wheel.vue"
+  import wheel from "./wheel/wheel.js"
+
+  export default {
+    components: {
+      WheelView
+    },
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    mounted() {
+      let self = this
+      this.wheel = new wheel(self, {
+        areaNumber: 8,
+        speed: 16,
+        awardNumer: 1,
+        mode: 1,
+        callback: () => {
+         console.log('success');
+        }
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/wheel/Wheel.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="wheel_wp">
+      <div v-if="mode == 1">
+        <img src="../../components/wheel/images/dial_bg.png"/>
+        <!--style="-webkit-transform:rotate({{deg}}deg) translateZ(0);transform:rotate({{deg}}deg) translateZ(0)"-->
+
+        <div class="wheel_pointer"
+             @click="start">
+          <img src="../../components/wheel/images/dial_pointer.png"/>
+        </div>
+      </div>
+      <div v-if="mode == 2">
+        <!--style="-webkit-transform:rotate({{deg}}deg) translateZ(0);transform:rotate({{ deg}}deg) translateZ(0)"-->
+        <img src="../../components/wheel/images/dial_bg.png"
+        />
+        <div class="wheel_pointer" @click="start">
+          <img src="../../components/wheel/images/dial_pointer.png"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        mode: 1
+      }
+    },
+    methods: {
+      start() {
+        console.log('start in vue');
+      }
+    }
+  }
+
+</script>
+<style scoped>
+
+  .wheel_wp {
+    width: 250px;
+    height: 250px;
+    position: relative;
+    margin: 0 auto;
+  }
+
+  .wheel_wp image {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
+  .wheel_wp .wheel_pointer {
+    position: absolute;
+    width: 75px;
+    height: 75px;
+    top: 50%;
+    left: 50%;
+    margin: -37.5px 0 0 -37.5px;
+    transform-origin: 50% 50%;
+  }
+
+  .wheel_wp .wheel_pointer image {
+    position: absolute;
+    width: 75px;
+    height: 119px;
+    left: 0;
+    top: -32px;
+  }
+
+</style>
